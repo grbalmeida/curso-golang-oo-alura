84dbc2d072fb3c3399e5b47ceb916982d8bd45d7
gilvan <g.r.almeida@live.com>
2020-08-05 22:38:40 -0300

    Variáveis e tipos

---
+++ src/banco/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	var titular string = "Guilherme"
+	var numeroAgencia int = 589
+	var numeroConta int = 123456
+	var saldo float64 = 125.50
+
+	fmt.Println(titular, numeroAgencia, numeroConta, saldo)
+
+	var titular2 string = "Luciene"
+	var numeroAgencia2 int = 555
+	var numeroConta2 int = 111333
+	var saldo2 float64 = 200
+
+	fmt.Println(titular2, numeroAgencia2, numeroConta2, saldo2)
+}
