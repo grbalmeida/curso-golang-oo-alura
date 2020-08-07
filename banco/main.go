ef6cd030f7eb33d2d68133a806750cea58d995cb
gilvan <g.r.almeida@live.com>
2020-08-07 05:13:27 -0300

    Conta poupança

--- banco/main.go
+++ banco/main.go
@@ -7,8 +7,10 @@ import (
 )
 
 func main() {
-	contaExemplo := contas.ContaCorrente{}
-	contaExemplo.Depositar(100)
+	contaDoDenis := contas.ContaPoupanca{}
+	contaDoDenis.Depositar(100)
+	contaDoDenis.Sacar(55)
+	contaDoDenis.Sacar(5000)
 
-	fmt.Println(contaExemplo.ObterSaldo())
+	fmt.Println(contaDoDenis.ObterSaldo())
 }
