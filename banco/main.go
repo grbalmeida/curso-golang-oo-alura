91e8dfdf546fd4fa6c25be3e0be785cc6f4d45b4
gilvan <g.r.almeida@live.com>
2020-08-07 05:01:47 -0300

    Alterando a visibilidade

--- banco/main.go
+++ banco/main.go
@@ -3,12 +3,12 @@ package main
 import (
 	"fmt"
 
-	"github.com/grbalmeida/curso-golang-oo-alura/banco/clientes"
 	"github.com/grbalmeida/curso-golang-oo-alura/banco/contas"
 )
 
 func main() {
-	clienteBruno := clientes.Titular{Nome: "Bruno", CPF: "123.123.123.12", Profissao: "Desenvolvedor Go"}
-	contaDoBruno := contas.ContaCorrente{Titular: clienteBruno, NumeroAgencia: 123, NumeroConta: 123456, Saldo: 100}
-	fmt.Println(contaDoBruno)
+	contaExemplo := contas.ContaCorrente{}
+	contaExemplo.Depositar(100)
+
+	fmt.Println(contaExemplo.ObterSaldo())
 }
