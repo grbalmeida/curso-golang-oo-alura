85d0b8df01daa8769e89533c978992c4548f8bd4
gilvan <g.r.almeida@live.com>
2020-08-07 04:56:50 -0300

    Tipos aninhados

--- banco/main.go
+++ banco/main.go
@@ -3,22 +3,12 @@ package main
 import (
 	"fmt"
 
+	"github.com/grbalmeida/curso-golang-oo-alura/banco/clientes"
 	"github.com/grbalmeida/curso-golang-oo-alura/banco/contas"
 )
 
 func main() {
-	contaDaSilvia := contas.ContaCorrente{Titular: "Silvia", Saldo: 300}
-	contaDoGustavo := contas.ContaCorrente{Titular: "Gustavo", Saldo: 100}
-
-	fmt.Println(contaDaSilvia)
-	fmt.Println(contaDoGustavo)
-
-	status := contaDaSilvia.Transferir(200, &contaDoGustavo)
-	fmt.Println(status, contaDaSilvia, contaDoGustavo)
-
-	status = contaDoGustavo.Transferir(50, &contaDaSilvia)
-	fmt.Println(status, contaDaSilvia, contaDoGustavo)
-
-	status = contaDoGustavo.Transferir(250, &contaDaSilvia)
-	fmt.Println(status, contaDaSilvia, contaDoGustavo)
+	clienteBruno := clientes.Titular{Nome: "Bruno", CPF: "123.123.123.12", Profissao: "Desenvolvedor Go"}
+	contaDoBruno := contas.ContaCorrente{Titular: clienteBruno, NumeroAgencia: 123, NumeroConta: 123456, Saldo: 100}
+	fmt.Println(contaDoBruno)
 }
