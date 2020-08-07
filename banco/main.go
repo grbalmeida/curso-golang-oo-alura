15f758f756c7b061a426c85a222ead97400efd62
gilvan <g.r.almeida@live.com>
2020-08-07 05:22:30 -0300

    Interface

--- banco/main.go
+++ banco/main.go
@@ -6,11 +6,26 @@ import (
 	"github.com/grbalmeida/curso-golang-oo-alura/banco/contas"
 )
 
+func PagarBoleto(conta verificarConta, valorDoBoleto float64) {
+	conta.Sacar(valorDoBoleto)
+}
+
+type verificarConta interface {
+	Sacar(valor float64) string
+}
+
 func main() {
 	contaDoDenis := contas.ContaPoupanca{}
 	contaDoDenis.Depositar(100)
-	contaDoDenis.Sacar(55)
-	contaDoDenis.Sacar(5000)
+	PagarBoleto(&contaDoDenis, 60)
 
 	fmt.Println(contaDoDenis.ObterSaldo())
+
+	contaDaLuisa := contas.ContaCorrente{}
+	contaDaLuisa.Depositar(500)
+	PagarBoleto(&contaDaLuisa, 400)
+	PagarBoleto(&contaDaLuisa, -400)
+	PagarBoleto(&contaDaLuisa, 1000)
+
+	fmt.Println(contaDaLuisa.ObterSaldo())
 }
