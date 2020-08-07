93e784e1e6b865905281b3a0b6b1ed2476fd3cde
gilvan <g.r.almeida@live.com>
2020-08-07 04:50:25 -0300

    Composição de structs

--- banco/contas/contaCorrente.go
+++ banco/contas/contaCorrente.go
@@ -1,7 +1,11 @@
 package contas
 
+import (
+	"github.com/grbalmeida/curso-golang-oo-alura/banco/clientes"
+)
+
 type ContaCorrente struct {
-	Titular       string
+	Titular       clientes.Titular
 	NumeroAgencia int
 	NumeroConta   int
 	Saldo         float64
